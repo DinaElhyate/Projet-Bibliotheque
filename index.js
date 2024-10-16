f15722bde2777602d9168c48d3628b0682b338ea
dinaelhyate <dinaelhyate@gmail.com>
2024-10-16 22:52:50 +0100

    first commit add api register

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+import express from "express";
+import authRoutes from "./routes/auth.js"; 
+import cookieParser from "cookie-parser"; 
+
+const app = express();
+
+app.use(express.json()); 
+app.use(cookieParser()); 
+
+app.use("/api/auth", authRoutes); 
+
+app.listen(8800, () => {
+  console.log("Connected on port 8800!");
+});
